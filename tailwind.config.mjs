013055c3362dd1f38566f6d1be3074d2256b7c22
Luis Arzola <luisarzola3@gmail.com>
2024-08-02 16:53:22 -0700

    Finished news section and started Gods section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,10 @@ export default {
                 montserrat: ["Montserrat", "sans-serif"],
                 prata: ["Prata", "serif"],
             },
+            colors: {
+                "white-opacity-10": "rgba(255,255,255,0.1)",
+                primary: "rgba(235, 218, 156)",
+            },
         },
     },
     plugins: [],
